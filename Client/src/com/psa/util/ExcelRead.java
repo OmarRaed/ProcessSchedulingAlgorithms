788f5f3477df374b5e172717ce7c7e8bfd5cc200
DukeOmar <raidomar5@gmail.com>
2019-05-30 07:08:21 +0200

    Added Excel Read & Write Classes

---
+++ Client/src/com/psa/util/ExcelRead.java
@@ -0,0 +1,70 @@
+package com.psa.util;
+
+import com.psa.model.Process;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+public class ExcelRead {
+
+    public List<Process> readProcesses(String filePath) throws FileNotFoundException {
+
+        //create list of Process
+        List<Process> processes = new ArrayList<>();
+
+        //Create an object of File class to open xls file
+        File src = new File(filePath);
+
+        //Create an object of FileInputStream class to read excel file
+        FileInputStream fis = new FileInputStream(src);
+
+        try {
+            //Create an object of HSSFWorkbook
+            HSSFWorkbook wb = new HSSFWorkbook(fis);
+            //specify first sheet in wb
+            HSSFSheet Sheet1 = wb.getSheetAt(0);
+
+            //Create a loop to set cells values in every row in
+            for (int i = 1; i < Sheet1.getLastRowNum(); i++) {
+
+                //create object from process
+                Process process = new Process();
+
+                //set process name
+                process.setProcessName(Sheet1.getRow(i)
+                                             .getCell(0)
+                                             .getStringCellValue());
+
+                //set process arrival time
+                process.setArriveTime((int) Sheet1.getRow(i)
+                                                  .getCell(1)
+                                                  .getNumericCellValue());
+
+                //set process burst time
+                process.setBurstTime((int) Sheet1.getRow(i)
+                                                 .getCell(2)
+                                                 .getNumericCellValue());
+
+                //set process deadline
+                process.setDeadline((int) Sheet1.getRow(i)
+                                                .getCell(3)
+                                                .getNumericCellValue());
+
+                processes.add(process);
+
+            }
+
+        } catch (IOException e) {
+        }
+    }
+
+}
---
+++ Client/src/com/psa/util/ExcelWrite.java
@@ -0,0 +1,66 @@
+package com.psa.util;
+
+import com.psa.model.Process;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+public class ExcelWrite {
+
+    public void writeProcesses(Queue<Process> processes, String algorithmTitle, String filePath) throws IOException {
+
+        HSSFWorkbook workbook = new HSSFWorkbook(); //create new workbook
+        HSSFSheet sheet = workbook.createSheet("ProcessSheet"); //create new sheet
+        HSSFRow row = sheet.createRow(0); //create new row for algorithm name
+
+        row.createCell(0).setCellValue("Policy : " + algorithmTitle);
+
+        //write columns titles
+        row = sheet.createRow(1); //create new row for titles
+        row.createCell(0).setCellValue("TASK");
+        row.createCell(1).setCellValue("START TIME");
+        row.createCell(2).setCellValue("END TIME");
+        row.createCell(3).setCellValue("DURATION");
+        row.createCell(4).setCellValue("STATUS");
+
+        //initialize row number
+        int rowNum = 2;
+
+        Queue<Process> outputProcesses = new LinkedList<>(processes);
+
+        while (!outputProcesses.isEmpty()) {
+
+            HSSFRow processRow = sheet.createRow(rowNum); //create new row
+            rowNum++;
+
+            Process process = new Process();
+
+            processRow.createCell(0).setCellValue(process.getProcessName());
+            processRow.createCell(1).setCellValue(process.getStartTime());
+            processRow.createCell(2).setCellValue(process.getEndTime());
+            processRow.createCell(3).setCellValue(process.getBurstTime());
+
+            if (process.getDeadline() > process.getEndTime())
+                processRow.createCell(4).setCellValue("S");
+            else
+                processRow.createCell(4).setCellValue("F");
+
+        }
+
+        //write to excel file
+        FileOutputStream out = new FileOutputStream(filePath + ".xls");
+        workbook.write(out);
+
+        workbook.close(); //close workbook
+        out.close(); //close file stream
+
+    }
+
+}
