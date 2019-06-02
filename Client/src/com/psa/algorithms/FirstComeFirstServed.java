4f66cbc46aedef74d81a0c9961663652a47e6cff
DukeOmar <raidomar5@gmail.com>
2019-06-02 12:26:58 +0200

    Commented FirstComeFirstServed Algorithm class

--- Client/src/com/psa/algorithms/FirstComeFirstServed.java
+++ Client/src/com/psa/algorithms/FirstComeFirstServed.java
@@ -12,29 +12,44 @@ public class FirstComeFirstServed {
 
     public Queue<Process> runAlgorithm(List<Process> processes) {
 
+        //initialize empty process queue
         Queue<Process> outputQueue = new LinkedList<>();
         Queue<Process> readyQueue = new LinkedList<>();
-
-        Collections.sort(processes);
-
-        int currentTime = 0;
         
         //get a copy of the list
         List<Process> unarrivedProcesses = new ArrayList<>(processes);
+
+        //sort processes by arrival time
+        Collections.sort(unarrivedProcesses);
         
+        //set the remaining time for each process equals the burst time
+        for(Process p : unarrivedProcesses)
+            p.setRemainingTime(p.getBurstTime());
+        
+        //initialize currentTime
+        int currentTime = 0;
+
+        //loop until all process finishes
         while (unarrivedProcesses.size() != 0 || !readyQueue.isEmpty()) {
 
+            //if ready queue is not empty
             if (!readyQueue.isEmpty()) {
 
+                //get process at the head of the ready queue
                 Process currentProcess = readyQueue.poll();
 
+                //set process start time equals current time
                 currentProcess.setStartTime(currentTime);
 
+                //increment current time by process burst time
                 currentTime += currentProcess.getBurstTime();
 
+                //set process end time equals current time
                 currentProcess.setEndTime(currentTime);
 
+                //add this process to the output queue
                 outputQueue.add(currentProcess);
+                
             }else if(currentTime != 0){
                 
                 //get time when the next process will arrive
@@ -42,6 +57,7 @@ public class FirstComeFirstServed {
                 
                 //create idle process
                 Process idleProcess = new Process();
+                
                 //set process data
                 idleProcess.setProcessName("PIDLE");
                 idleProcess.setStartTime(currentTime);
@@ -59,17 +75,22 @@ public class FirstComeFirstServed {
                 currentTime = nextProcessArrivalTime;
             }
 
-
+            //loop for all unarrived processes
             for (int i = 0; i < unarrivedProcesses.size(); i++) {
 
+                //get process i
                 Process process = unarrivedProcesses.get(i);
-
+    
+                //if process arrived
                 if (process.getArriveTime() <= currentTime) {
 
+                    //then add it to the ready queue
                     readyQueue.add(process);
 
+                    //and remove it from the unarrived queue
                     unarrivedProcesses.remove(i);
 
+                    //and decrement i
                     i--;
 
                 }
